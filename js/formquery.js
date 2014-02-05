04c1cc7f2189a338d7be882e592292d6eb8f3c32
Angelina Velinska <angelina.velinska@coremedia.com>
2014-02-05 16:52:07 +0100

    send form doesnt work yet

---
+++ js/formquery.js
@@ -0,0 +1,26 @@
+/* ausführen, wenn html-seite geladen wurde */
+$(document).ready(function () {
+  /* sendung formular abfangen */
+  $("#contactform").submit(function () {
+
+    /* ajax objekt zum aufruf & versand an das skript
+     'name' und 'email' sind in der data-zeile die variablen für das php-skript */
+    $.ajax({
+      type: "POST",
+      url: "formsend.php",
+      data: "name=" + $("#name").val() + "&email=" + $("#email").val() + "&message=" + $("#message").val(),
+      success: function (msg) {
+        /* form-div verstecken, seite nachladen & wieder einblenden (2000 ms) */
+        $("#formdiv").hide().load("thankyou.html").fadeIn(2000);
+      }
+    });
+
+    /* wichtig!
+     sonst schickt der browser das formular ab und
+     und ruft die seite auf die bei action="" hinterlegt wurde.
+     dann verlässt er nämlich die bisherige seite... */
+    return false;
+
+  });
+
+});
---
+++ php/formsend.php
@@ -0,0 +1,43 @@
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$errors = '';
+$myemail = 'an.velinska@gmail.com';
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$test=$_POST['name'];
+echo $test;
+
+$email_address = $_POST['email'];
+$test=$_POST['email'];
+echo $test;
+
+$message = $_POST['message'];
+$test=$_POST['message'];
+echo $test;
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: thankyou.html');
+}
