d980938cebe161d696fcb7ce6872e75eb3718bb2
Angelina Velinska <angelina.velinska@coremedia.com>
2014-03-16 22:12:24 +0100

    use the correct form fields for the php function

--- php/formsend.php
+++ php/formsend.php
@@ -1,3 +1,4 @@
+name=abcd&email=ng_agi@abv.bg&message=ala bala
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
@@ -28,14 +29,14 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
-if( empty($errors))
+if (empty($errors))
 {
-$to = $myemail;
+$to = 'an.velinska@gmail.com';
 $email_subject = "Contact form submission: $name";
 $email_body = "You have received a new message. ".
 " Here are the details:\n Name: $name \n ".
 "Email: $email_address\n Message \n $message";
-$headers = "From: $myemail\n";
+$headers = "From: $to\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
